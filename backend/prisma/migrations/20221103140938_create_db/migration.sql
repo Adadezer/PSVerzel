0965cc6f19b58e99c4920b6dcd4de8bc91a4f60e
Adadezer <adadezer@gmail.com>
2022-11-03 11:18:04 -0300

    build(backend): :arrow_up: instalação dependencia jwt

--- backend/prisma/migrations/20221103140938_create_db/migration.sql
+++ backend/prisma/migrations/20221103140938_create_db/migration.sql
@@ -1,22 +1,22 @@
 -- CreateTable
-CREATE TABLE `Vehicles` (
+CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `brand` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `image` VARCHAR(191) NOT NULL,
-    `carModel` VARCHAR(191) NOT NULL,
-    `userId` INTEGER NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
 
+    UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `Vehicles` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `brand` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(255) NOT NULL,
+    `carModel` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
